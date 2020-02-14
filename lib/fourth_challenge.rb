2c07b166cdb7516a029e4d1a5a63eccf2f15fb3d
Matt Kuras <mattkuras@gmail.com>
2020-02-14 21:06:31 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -10,8 +10,7 @@ def fourth_challenge
         attitude: "worried"},
         {name: "Mercutio",
         age: "18",
-        attitude: "hot-headed"
-        }
+        attitude: "hot-headed"}
       ]
    }, 
    :capulet => {
@@ -19,7 +18,12 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        #your hashes here!
+        {name: "Steven",
+        age: "30",
+        attitude: "confused"}
+        {name: "Nurse",
+        age: "44",
+        attitude: "worried"}
       ]
    }
 }
