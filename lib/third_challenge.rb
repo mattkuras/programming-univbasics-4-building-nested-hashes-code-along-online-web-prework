0fe5a8046ee173ab09d025e6cf82fec31d769548
Matt Kuras <mattkuras@gmail.com>
2020-02-14 20:57:54 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -22,7 +22,8 @@ def third_challenge
         age: "50"
       },
       :matriarch => {
-        #your key/value pairs here
+        name: "Lady Capuet",
+        age: "51"
         },
       :heroine => {
         #your key/value pairs here
